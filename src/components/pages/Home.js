bf48e555a9d4923c626c0801654355747aed02f9
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-08-10 22:13:59 +0300

    ((

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TrendingList from "components/TrendingList";
 
 const Home = () =>{
-     const [movies, setMovies] = useState([
-          'movie-1',
-          'movie-2',
-          'movie-3',
-          'movie-4',
-          'movie-5'
-     ]);
-     //   useEffect(() => {
-     //      HTTP request make, if you need
-     //   }, [])
+     const [movies, setMovies] = useState([]);
+
+       useEffect(() => {
+          const options = {
+               method: 'GET',
+               headers: {
+                 accept: 'application/json',
+                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDk2NDE3YzZmNTdkYmE2NjM3Yjg5ZTA1MTlkZjhjMCIsInN1YiI6IjY0ZDQ5NmNiYmYzMWYyMDFjZTY3NTk4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oUfYFt39LV0c4K3VhOIRVjLifgRGqlfBdeSL9BhgEbU'
+               }
+             };
+             
+             fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+               .then(response => response.json())
+               .then(response => response.results.map(resalt => resalt.title))
+               .catch(err => console.error(err));
+       }, [])
      
           return <div>
                    <h1>Trending today</h1>
@@ -19,4 +25,4 @@ const Home = () =>{
                  </div>
      };
      
-     export default Home;
+export default Home;
